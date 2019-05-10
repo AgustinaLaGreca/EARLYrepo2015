c5d793d5081ed18c48f4c6cdced0192b5d1751d2
AgustinaLaGreca <agustina.lagrecas@gmail.com>
2019-05-10 18:48:31 +0200

    Temporary commit

--- FrequencyStepperBPN.m
+++ FrequencyStepperBPN.m
@@ -1,44 +1,39 @@
-function Modsweep=FrequencyStepperBPN(T, EXP, Prefix);
-% SAMpanel - generic amplitude modulation panel for stimulus GUIs.
-%   S=SAMpanel(Title, EXP) returns a GUIpanel M allowing the 
-%   user to specify a fixed sinusiodal amplitude modulation to be applied 
-%   to all the stimuli of a series. Guipanel S has title Title. EXP is the 
-%   experiment definition, from which the number of DAC channels used 
-%   (1 or 2) is determined.
+function Fsweep=FrequencyStepperARMIN(T, EXP, Prefix, Flag, Flag2);
+% FrequencyStepperARMIN - generic frequency stepper panel for stimulus GUIs.
+%   F=FrequencyStepperARMIN(Title, EXP) returns a GUIpanel F allowing the 
+%   user to specify a series of frequencies, using either be logarithmic or
+%   linear spacing.  The Guipanel F has title Title. EXP is the experiment 
+%   definition, from which the number of DAC channels used (1 or 2) is
+%   determined.
 %
 %   The paramQuery objects contained in F are named: 
-%      StartModFreq: starting frequency in Hz
-%  StepModFrequency: step in Hz or Octaves (toggle unit)
-%   EndModFrequency: end frequency in Hz
-%     AdjustModFreq: toggle selecting which of the above params to adjust
+%         StartFreq: starting frequency in Hz
+%     StepFrequency: step in Hz or Octaves (toggle unit)
+%      EndFrequency: end frequency in Hz
+%        AdjustFreq: toggle selecting which of the above params to adjust
 %                    in case StepFrequency does not fit exactly.
-%    ModFreqTolMode: toggle selecting whether frequencies should be
+%       FreqTolMode: toggle selecting whether frequencies should be
 %                    realized exactly, or whether memory-saving rounding is
 %                    allowed.
-%           ModDepth: modulation depth in % (100%==full AM)
-%      ModStartPhase: starting phase of modulation in Cycles (0=cos)
+%       LowPolarity: the sign of the correlation below the flip frequency
+%          CorrUnit: the varied channel (I/C), of which part of the
+%                    spectrum is modified
 %
-%   SAMpanel is a helper function for stimulus definitions like stimdefFS.
+%   FrequencyStepperARMIN is a helper function for stimulus generators like 
+%   makestimARMIN.
 % 
-%   M=SAMpanel(Title, EXP, Prefix) prepends the string Prefix
-%   to the paramQuery names, e.g. ModFreq -> NoiseModFreq, etc.
-%   Default Prefix is '', i.e., no prefix.
+%   F=FrequencyStepperARMIN(Title, ChanSpec, Prefix) prepends the string Prefix
+%   to the paramQuery names, e.g. StartFreq -> ModStartFreq, etc.
 %
-%   M=SAMpanel(Title, EXP, Prefix, 1) also includes a Theta query. Theta is
-%   the modulation angle.
+%   Use EvalFrequencyStepperARMIN to read the values from the queries and to
+%   compute the actual frequencies specified by the above step parameters.
 %
-%   Use EvalSAMpanel to read the values from the queries.
-%
-%   See StimGUI, GUIpanel, EvalSAMpanel, stimdefFS.
+%   See StimGUI, GUIpanel, EvalFrequencyStepperARMIN, makestimARMIN.
 
-[Pref, IncTheta, NoTolerance] = arginDefaults('Prefix/IncludeTheta/NoTol', '', 1, 0);
-if (nargin < 3), Prefix = Pref; end
-if (nargin < 4), IncludeTheta = IncTheta; end
-if (nargin < 5), NoTol = NoTolerance; end
-    
+[Prefix, Flag, Flag2] = arginDefaults('Prefix/Flag/Flag2', '');
 
-% # DAC channels fixes the allowed multiplicity of user-specied numbers
-if isequal('Both', EXP.AudioChannelsUsed), 
+% # DAC channels and Flag2 determines the allowed multiplicity of user-specied numbers
+if isequal('Both', EXP.AudioChannelsUsed) && ~isequal('nobinaural', Flag2), 
     Nchan = 2;
     PairStr = ' Pairs of numbers are interpreted as [left right].';
 else, % single Audio channel
@@ -47,48 +42,38 @@ else, % single Audio channel
 end
 ClickStr = ' Click button to select ';
 
-% ---Modulation
-Modsweep = GUIpanel('Modsweep', T);
-StartModFreq = ParamQuery([Prefix 'StartModFreq'], 'start freq:', '15000.5 15000.5', 'Hz', ...
-    'rreal/nonnegative', ['Starting frequency of series.' PairStr], Nchan);
-StepModFreq = ParamQuery([Prefix 'StepModFreq'], 'step freq:', '12000', {'Hz' 'Octave'}, ...
+%==========frequency GUIpanel=====================
+Fsweep = GUIpanel('Fsweep', T);
+StartFreq = ParamQuery([Prefix 'StartFreq'], 'start:', '15000.5 15000.5', 'Hz', ...
+    'rreal/positive', ['Starting frequency of series.' PairStr], Nchan);
+StepFreq = ParamQuery([Prefix 'StepFreq'], 'step:', '12000', {'Hz' 'Octave'}, ...
     'rreal/positive', ['Frequency step of series.' ClickStr 'step units.'], Nchan);
-EndModFreq = ParamQuery('EndModFreq', 'end freq:', '12000.1 12000.1', 'Hz', ...
-    'rreal/nonnegative', ['Last frequency of series.' PairStr], Nchan);
-AdjustModFreq = ParamQuery([Prefix 'AdjustModFreq'], 'adjust:', '', {'none' 'start' 'step' 'end'}, ...
+EndFreq = ParamQuery([Prefix 'EndFreq'], 'end:', '12000.1 12000.1', 'Hz', ...
+    'rreal/positive', ['Last frequency of series.' PairStr], Nchan);
+AdjustFreq = ParamQuery([Prefix 'AdjustFreq'], 'adjust:', '', {'none' 'start' 'step' 'end'}, ...
     '', ['Choose which parameter to adjust when the stepsize does not exactly fit the start & end values.'], 1,'Fontsiz', 8);
-Tol = ParamQuery([Prefix 'ModFreqTolMode'], 'acuity:', '', {'economic' 'exact'}, '', [ ...
+Tol = ParamQuery([Prefix 'FreqTolMode'], 'acuity:', '', {'economic' 'exact'}, '', [ ...
     'Exact: no rounding applied;', char(10), 'Economic: allow slight (<1 part per 1000), memory-saving rounding of frequencies;'], ...
     1, 'Fontsiz', 8);
+LowPolarity = ParamQuery('LowPolarity', 'low polarity:', '', {'+' '-'}, ...
+    '', 'Sign of correlation below flip frequency. Correlation above this frequency will be the opposite.');
 
-% ModDepth = ParamQuery([Prefix 'ModDepth'], ...
-%     'depth:', '20.5 33.3', '%', 'rreal/nonnegative', ...
-%     ['Modulation depth.' PairStr], Nchan);
-% ModStartPhase = ParamQuery([Prefix 'ModStartPhase'], ...
-%     'phase:', '-0.25 -0.33', 'Cycle', 'rreal', ...
-%     ['Starting phase of modulation. Zero means cosine phase.' PairStr], Nchan);
-% Theta = ParamQuery([Prefix 'ModTheta'], ...
-%     'theta:', '-0.25 -0.33', 'Cycle', 'rreal', ...
-%     ['Modulation angle. Theta = 0 results in AM; Theta=0.25 is QFM; other values give mixed modulation.' PairStr], Nchan);
 
 CutoffSide = ParamQuery('CutoffSide', 'Select which edge to vary:', '', {'L' 'H'}, ...
     '', 'L varies the lower side, making a HPF, and vice versa.');
 
-Modsweep = add(Modsweep, StartModFreq);
-Modsweep = add(Modsweep, StepModFreq, alignedwith(StartModFreq));
-Modsweep = add(Modsweep, EndModFreq, alignedwith(StepModFreq));
-Modsweep = add(Modsweep, AdjustModFreq, nextto(StepModFreq), [10 0]);
-Modsweep = add(Modsweep, CutoffSide, below(EndModFreq));
-% Modsweep = add(Modsweep,ModDepth, alignedwith(EndModFreq));
-% Modsweep = add(Modsweep,ModStartPhase, alignedwith(ModDepth));
-% Modsweep = add(Modsweep,ModITD, alignedwith(ModStartPhase));
 
-% if IncludeTheta,
-%     Modsweep = add(Modsweep,Theta, 'aligned', [0 -5]);
-% end
-% if ~isequal('notol', NoTol),
-%     Modsweep = add(Modsweep, Tol, alignedwith(AdjustModFreq) , [0 -10]);
+Fsweep = add(Fsweep, StartFreq);
+Fsweep = add(Fsweep, StepFreq, alignedwith(StartFreq));
+Fsweep = add(Fsweep, EndFreq, alignedwith(StepFreq));
+Fsweep = add(Fsweep, AdjustFreq, nextto(StepFreq), [10 0]);
+Fsweep = add(Fsweep, LowPolarity, below(EndFreq), [0 2]);
+Fsweep = add(Fsweep, CutoffSide, nextto(LowPolarity), [15 0]);
+if ~isequal('notol', Flag),
+    Fsweep = add(Fsweep, Tol, alignedwith(AdjustFreq) , [0 -10]);
 end
+
+
 
 
 
