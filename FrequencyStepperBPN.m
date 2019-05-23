315dde2df93ed9e87b79ecba4c783fe38d504221
AuRLabLeuven <AuRLabLeuven@gmail.com>
2019-05-23 16:09:15 +0200

    Temporary commit

--- FrequencyStepperBPN.m
+++ FrequencyStepperBPN.m
@@ -57,8 +57,9 @@ AdjustFreq = ParamQuery([Prefix 'AdjustFreq'], 'adjust:', '', {'none' 'start' 's
 Tol = ParamQuery([Prefix 'FreqTolMode'], 'acuity:', '', {'economic' 'exact'}, '', [ ...
     'Exact: no rounding applied;', char(10), 'Economic: allow slight (<1 part per 1000), memory-saving rounding of frequencies;'], ...
     1, 'Fontsiz', 8);
-CorrUnit = ParamQuery('CorrUnit', 'To Select the Channel:', '', {'I' 'C'}, ...
-    '', ' [debugging].');
+% CorrUnit = ParamQuery('CorrUnit', 'To Select the Channel:', '', {'I' 'C'}, ...
+%     '', ' [debugging].');                 
+% There is no varied channel in BPN
 
 
 CutoffSide = ParamQuery('CutoffSide', 'Select which edge to vary:', '', {'L' 'H'}, ...
@@ -70,7 +71,7 @@ Fsweep = add(Fsweep, StepFreq, alignedwith(StartFreq));
 Fsweep = add(Fsweep, EndFreq, alignedwith(StepFreq));
 Fsweep = add(Fsweep, AdjustFreq, nextto(StepFreq), [10 0]);
 Fsweep = add(Fsweep, CutoffSide, below(EndFreq), [0 0]);
-Fsweep = add(Fsweep, CorrUnit, below(CutoffSide), [0 0]);
+% Fsweep = add(Fsweep, CorrUnit, below(CutoffSide), [0 0]);        
 if ~isequal('notol', Flag),
     Fsweep = add(Fsweep, Tol, alignedwith(AdjustFreq) , [0 -10]);
 end
