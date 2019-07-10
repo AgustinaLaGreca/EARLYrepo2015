3ba89801a21b27b9f6c3ed465e5216b50239db5c
AuRLabLeuven <AuRLabLeuven@gmail.com>
2019-07-10 14:13:28 +0200

    ZWP 2
    ZWP analysis tools, most of them in DataAnalysis/ZWPdata folder

--- vs10/DataAnalysis/ZWPdata/dp2input.m
+++ vs10/DataAnalysis/ZWPdata/dp2input.m
@@ -1,19 +1,37 @@
-function [D, Z] = dp2input(d, Cdelay)
+function [S, Z] = dp2input(D, Cdelay)
 % dp2input - IHC input from DP2 spectrum  of spike train evoked by zwuis stim
 %  [D, Z] = dp2input(d, Cdelay)
 %        d: dataset (ZW or ZWP stimulus).
 %   Cdelay: ms delay compensation (needed for phase unwrapping)
+
+Sdir = 'C:\usr\Marcel\ZWP_pilot\ProcData';
 
 AlphaCrit = 0.001; % max p value for Rayleigh test
 NminOccur = 3; % minimum occurrence of primary as parent of DP2s
 
 doPlot = nargout<1;
 
-Cdelay = arginDefaults('Cdelay',3); 
+Cdelay = arginDefaults('Cdelay',3);
 
-Z = zwuisana(d.Stim, digichan(d,1)); 
-St = d.Stim; 
-SPT = spiketimes(d);
+for ii=1:numel(D),
+    d = D(ii);
+    SPT = spiketimes(d);
+    St = d.Stim;
+    S(ii) = local_process(d, AlphaCrit, NminOccur, Cdelay, Sdir);
+    disp(trimspace(num2str(round(10*S(ii).DL)/10)));
+end
+
+
+if doPlot,
+    local_plot(S);
+end
+
+
+
+%============================================
+function S = local_process(d, AlphaCrit, NminOccur, Cdelay, Sdir);
+Z = zwuisana(d.Stim, digichan(d,1));
+St = d.Stim;
 Nsp = sum(Z.R);% # spikes
 f2i = @(f)1+round(1e3*f/Z.df); % kHz => index in spectrum
 
@@ -46,33 +64,27 @@ Mw = cat(1,DP2.weight); % weight matrix
 % phases
 PhaseRef = Mw*St.StartPhase.';
 PhaseDiff = cangle(VSdiff) - PhaseRef + Cdelay*Fdiff;
-
-
 % solve MagInp from MagDiff = abs(Mw)*MagInp
 % MagInp1 = abs(Mw)\MagDiff;
 % MagInp1 = MagInp1 - nanmean(MagInp1);
-[MagInp, FitMag] = local_leftdiv(abs(Mw), MagDiff, NminOccur, false, Fdiff, [1 2 3 4 5]);
+% [MagInp, FitMag] = local_leftdiv(abs(Mw), MagDiff, NminOccur, false, Fdiff, [1 2 3 4 5]);
+[MagInp, FitMag] = local_leftdiv(abs(Mw), MagDiff, NminOccur, false, Fdiff, [1 ]);
 MagInp = MagInp - nanmean(MagInp);
-1;
-
 % solve PhaseInp from PhaseDiff = Mw*PhaseInp
 % PhaseInp = [Mw; ones(1, Nprim)]\[PhaseDiff; 0]; % addition "data" forcing sum(PhaseInp)=0, lifting rank deficiency
 PhaseInp = [];
-
-
-
+%=
 ID = d.ID;
-D = CollectInStruct(ID, '-param', AlphaCrit, VScrit, Nsp, Cdelay, ...
+DL = St.SPLjump - MagInp;
+S = CollectInStruct(ID, '-param', AlphaCrit, VScrit, Nsp, Cdelay, ...
     '-apple', Nprim, Fprim, VSprim, MagPrim, PhasePrim, PMprim, ...
     Fdiff, VSdiff, MagDiff, PhaseDiff, PhaseRef, ...
-    '-dp2apple', Ndp2, Mw, MagInp, FitMag, PhaseInp);
-
-if doPlot,
-    local_plot(D);
-end
+    '-dp2apple', Ndp2, Mw, MagInp, FitMag, PhaseInp, DL);
+FN = [name(ID.Experiment) '_irec_' num2str(ID.iDataset) '.zwp'];
+save(fullfile(Sdir, FN), 'S', 'St', 'Z', '-V6');
 
 
-%============================================
+
 function [X, F] = local_leftdiv(Mw, Data, NminOccur, forceZeroMean, Fpoly, PolyPowers);
 % solves X and C from Data = M*X + polyval(C, Fpoly)
 Ndata = numel(Data);
@@ -84,7 +96,7 @@ if isempty(PolyPowers), % make VDM  construction below will work
     PolyPowers = zeros(1,0);
 end
 if forceZeroMean,  % add constraint fixing sum(X)
-    M = [M; ones(1, size(M,2))]; 
+    M = [M; ones(1, size(M,2))];
     Data = [Data; 0];
 end
 VDM = bsxfun(@power, Zfreq, PolyPowers); % VanderMonde matrix for polynomial fit
@@ -97,23 +109,33 @@ X(q_not_rare) = XX;
 F = CollectInStruct(VDM, C, Zfreq, NminOccur, forceZeroMean, Fpoly, PolyPowers);
 
 
-function local_plot(D);
+function local_plot(DD, CLR);
 fh = figure;
 set(fh,'units', 'normalized', 'position', [0.0974 0.312 0.28 0.565]);
-subplot(2,1,1); 
-plot(D.Fdiff, D.MagDiff, 'o'); 
-FilterTrend = D.FitMag.VDM*D.FitMag.C;
-FilterTrend = FilterTrend-nanmean(FilterTrend)+ nanmean(D.MagDiff);
-[dpfreq, FilterTrend] = sortAccord(D.Fdiff, FilterTrend, D.Fdiff);
-xplot(dpfreq, FilterTrend, '-', 'markersize',4);
-
-xlabel('Envelope frequency (kHz)');
-ylabel('Magnitude (dB re VS=1)');
-subplot(2,1,2); 
-plot(D.Fprim, D.MagInp, '*')
-xlabel('Primary Frequency (kHz)');
-ylabel('Magnitude (dB)');
-
+ID = [DD.ID];
+irec = [ID.iDataset];
+%=
+for ii=1:numel(DD),
+    D = DD(ii);
+    subplot(2,1,1);
+    allC = get(gca, 'ColorOrder');
+    CLR = allC(1+rem(ii-1,7), :);
+    xplot(D.Fdiff, D.MagDiff, 'o', 'color', CLR);
+    FilterTrend = D.FitMag.VDM*D.FitMag.C;
+    FilterTrend = FilterTrend-nanmean(FilterTrend)+ nanmean(D.MagDiff);
+    [dpfreq, FilterTrend] = sortAccord(D.Fdiff, FilterTrend, D.Fdiff);
+    xplot(dpfreq, FilterTrend, '-', 'markersize',4, 'color', CLR);
+    %=
+    xlabel('Envelope frequency (kHz)');
+    ylabel('Magnitude (dB re VS=1)');
+    subplot(2,1,2);
+    xplot(D.Fprim, D.MagInp, '*-', 'color', CLR)
+    fenceplot(D.Fprim, ylim, 'color', 0.5*[1 1 1]);
+    xlabel('Primary Frequency (kHz)');
+    ylabel('Magnitude (dB)');
+end
+subplot(2,1,1);
+title([name(ID(1).Experiment) '  irec= '  trimspace(num2str([ID.iDataset]))] );
 
 
 
